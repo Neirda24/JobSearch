495cf963a021c865ccccd424fbcd46cd9af9ae60
Adrien Schaegis <adrien@phileasstravinarius.com>
2016-09-08 18:14:55 +0200

    [FEATURE] Add Edit company + small refacto

---
+++ src/AppBundle/Controller/AbstractController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\FormError;
+
+abstract class AbstractController extends Controller
+{
+    /**
+     * Message can be a translation key.
+     *
+     * {@inheritdoc}
+     */
+    protected function addFlash($type, $message, array $parameters = [], string $domain = 'messages')
+    {
+        $message = $this->get('translator')->trans($message, $parameters, $domain);
+        
+        parent::addFlash($type, $message);
+    }
+    
+    /**
+     * @param string $key
+     * @param array  $parameters
+     * @param string $domain
+     *
+     * @return FormError
+     */
+    protected function createFormError(string $key, array $parameters = [], string $domain = 'validators')
+    {
+        $message = $this->get('translator')->trans($key, $parameters, $domain);
+        
+        return new FormError($message);
+    }
+}
--- src/AppBundle/Controller/CompanyController.php
+++ src/AppBundle/Controller/CompanyController.php
@@ -6,18 +6,16 @@ use AppBundle\Entity\Company;
 use AppBundle\Form\CompanyType;
 use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 
-class CompanyController extends Controller
+class CompanyController extends AbstractController
 {
     /**
-     * @Route("/company/list", name="company_list")
+     * @Route("/company/list", name="company_list", methods={"GET"})
      */
-    public function listAction(Request $request)
+    public function listAction()
     {
-        $em = $this->getDoctrine()->getManager();
+        $em         = $this->getDoctrine()->getManager();
         $repository = $em->getRepository('AppBundle:Company');
         
         $companies = $repository->findAll();
@@ -28,32 +26,57 @@ class CompanyController extends Controller
     }
     
     /**
-     * @Route("/company/create", name="company_create")
+     * @Route("/company/create", name="company_create", methods={"GET", "POST"})
      */
     public function createAction(Request $request)
     {
         $company = new Company();
-        $form = $this->createForm(CompanyType::class, $company, [
+        $form    = $this->createForm(CompanyType::class, $company, [
             'method' => Request::METHOD_POST,
         ]);
         
         $form->handleRequest($request);
-    
+        
         if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
             $em->persist($company);
             try {
                 $em->flush();
-    
-                $this->get('session')->getFlashBag()->add('success', 'Successfuly inserted');
+                
+                $this->addFlash('success', 'Successfuly Inserted');
             } catch (UniqueConstraintViolationException $ucve) {
-                $error = new FormError($this->get('translator')->trans('error.company.already_exists', [], 'validators'));
+                $error = $this->createFormError('error.company.already_exists');
                 $form->addError($error);
             }
         }
         
         return $this->render('AppBundle:Company:create.html.twig', [
             'form' => $form->createView(),
+        ]);
+    }
+    
+    /**
+     * @Route("/company/edit/{id}", name="company_edit", methods={"GET", "PUT"})
+     */
+    public function editAction(Request $request, Company $company)
+    {
+        $form = $this->createForm(CompanyType::class, $company, [
+            'method' => Request::METHOD_PUT,
+        ]);
+        
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($company);
+            $em->flush();
+            
+            $this->addFlash('success', 'Successfuly updated');
+        }
+        
+        return $this->render('AppBundle:Company:edit.html.twig', [
+            'form' => $form->createView(),
+            'edit' => true,
         ]);
     }
 }
